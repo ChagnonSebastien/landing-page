44946a8601c8253a857894be41fe61894bec1fcc
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-07-18 08:15:05 -0400

    Removes methods now handled by the server in cloud functions

--- functions/src/blogMessages.ts
+++
@@ -1,9 +0,0 @@
-import * as express from 'express';
-
-const blogMessages = express();
-
-blogMessages.use('/', (request, response) => {
-  response.send("0.1");
-});
-
-export default blogMessages;
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,7 @@
 import * as functions from 'firebase-functions';
-import blogMessagesExpress from './blogMessages';
 import retrievePositionData from './retrievePositionData';
 
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-export const blogMessages = functions.https.onRequest(blogMessagesExpress);
-
-export const version = functions.https.onRequest((_, response) => response.send("0.1"));
-
-export const fetchLocations = functions.pubsub.schedule('*/5 06-21 * * *').onRun(retrievePositionData);
+export const fetchLocations = functions.pubsub.schedule('*/5 06-21 * * *').timeZone('America/Montreal').onRun(retrievePositionData);
