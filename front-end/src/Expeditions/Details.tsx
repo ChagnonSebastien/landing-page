e1fc1650f852659b8ce8f5d2d23e2e880499a155
Sebastien Chagnon <sebastien.chagnon@polymtl.ca>
2020-07-19 10:26:56 -0400

    Added a Google map with markers

--- front-end/src/Expeditions/Details.tsx
+++ front-end/src/Expeditions/Details.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, memo } from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-import { Row } from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 import { withRouter } from 'react-router-dom';
 import { Scatter } from 'react-chartjs-2';
 
@@ -10,12 +11,34 @@ import server from '../server';
 import axios from 'axios';
 import Vector2D from './Vector2D';
 
-const calculateHeightProfile = (locationHistory: SpotPing[]) => {
+const scaleToDistanceRatios = [
+  { zoom: 20, size: 1128.497220 },
+  { zoom: 19, size: 2256.994440 },
+  { zoom: 18, size: 4513.988880 },
+  { zoom: 17, size: 9027.977761 },
+  { zoom: 16, size: 18055.955520 },
+  { zoom: 15, size: 36111.911040 },
+  { zoom: 14, size: 72223.822090 },
+  { zoom: 13, size: 144447.644200 },
+  { zoom: 12, size: 288895.288400 },
+  { zoom: 11, size: 577790.576700 },
+  { zoom: 10, size: 1155581.153000 },
+  { zoom: 9 , size: 2311162.307000 },
+  { zoom: 8 , size: 4622324.614000 },
+  { zoom: 7 , size: 9244649.227000 },
+  { zoom: 6 , size: 18489298.450000 },
+  { zoom: 5 , size: 36978596.910000 },
+  { zoom: 4 , size: 73957193.820000 },
+  { zoom: 3 , size: 147914387.600000 },
+  { zoom: 2 , size: 295828775.300000 },
+  { zoom: 1 , size: 591657550.500000 }
+]
+
+const calculateHeightProfile = (locationHistory: SpotPing[]): Vector2D[] => {
   if (locationHistory.length === 0) {
     return [];
   }
 
-  console.log(locationHistory);
   let profile: Vector2D[] = [{x: 0, y: locationHistory[0].elevation}]
   let distanceTraveled = 0;
   for (let i = 1; i < locationHistory.length; i += 1) {
@@ -51,7 +74,7 @@ const ExpeditionDetails = withRouter((props: Props)  => {
     axios.all([
       server.get(`/expeditions/${expeditionId}`),
       server.get(`/expeditions/${expeditionId}/locationHistory/latest`),
-      server.get(`/expeditions/${expeditionId}/locationHistory`),
+      server.get(`/expeditions/${expeditionId}/locationHistory?date=2020-07-16`),
     ])
       .then(axios.spread((
         expeditionResponse,
@@ -68,39 +91,74 @@ const ExpeditionDetails = withRouter((props: Props)  => {
   }, [expeditionId]);
 
   return (
-    <Row>
+    <>
       <h1>{expedition?.name}</h1>
-      <Scatter
-        data={{
-          datasets: [{
-            data: heightProfile,
-            showLine: true,
-          }]
-        }}
-        options={{
-          legend: {
-            display: false
-          },
-          scales: {
-            xAxes: [{
-              position: 'bottom',
-              scaleLabel: {
-                display: true,
-                labelString: 'Distance Traveled (km)'
+      <Row>
+        <Col>
+          <LoadScript
+            googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_KEY}
+          >
+            {locationHistory
+              ? (
+                <GoogleMap
+                  mapContainerStyle={{
+                    width: '100%',
+                    height: '35rem'
+                  }}
+                  onLoad={map => {
+                    const windowRef: any = window;
+                    const bounds = new windowRef.google.maps.LatLngBounds();
+                    locationHistory?.forEach((point) => bounds.extend(new windowRef.google.maps.LatLng(point.location.latitude, point.location.longitude)));
+                    map.fitBounds(bounds);
+                  }}
+                >
+                  {locationHistory?.map((point: SpotPing) => (
+                    <Marker
+                      key={point.timestamp}
+                      position={{lat: point.location.latitude, lng: point.location.longitude}}
+                    />
+                  ))}
+                </GoogleMap>
+              ) : null}
+          </LoadScript>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Scatter
+            data={{
+              datasets: [{
+                data: heightProfile,
+                showLine: true,
+                lineTension: 0.2,
+              }]
+            }}
+            options={{
+              legend: {
+                display: false
+              },
+              scales: {
+                xAxes: [{
+                  position: 'bottom',
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'Distance Traveled (km)'
+                  }
+                }],
+                yAxes: [{
+                  position: 'bottom',
+                  scaleLabel: {
+                    display: true,
+                    labelString: 'Height (m)'
+                  }
+                }]
               }
-            }],
-            yAxes: [{
-              position: 'bottom',
-              scaleLabel: {
-                display: true,
-                labelString: 'Height (m)'
-              }
-            }]
-          }
-        }}
-      />
-    </Row>
+            }}
+          />
+        </Col>
+      </Row>
+    </>
   );
 });
 
-export default ExpeditionDetails;
+export default memo(ExpeditionDetails);
